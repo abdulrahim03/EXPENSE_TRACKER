fe58a5ff8d35c2a6d88b9134108813f9c6f25598
Abdul Rahim <abdul.r@credopay.com>
2024-09-17 12:02:43 +0530

    Dashboard Design completed

---
+++ src/script.js
@@ -0,0 +1,21 @@
+function showCommonPopUp(){
+    if(isMobileScreen()){
+        document.getElementById('commonIframe').style.display='block';
+        document.getElementById('dashboardcontentid').style.display='none';
+        document.getElementById('dashboardimgid').style.display='none';
+    }else{
+        document.getElementById('dashboardcontentid').style.display='block';
+        document.getElementById('commonIframe').style.display='block';
+        document.getElementById('dashboardimgid').style.display='block';
+    }
+}
+
+function closeCommonPopUp(){
+    document.getElementById('commonIframe').style.display='none';
+    document.getElementById('dashboardcontentid').style.display='block';
+    document.getElementById('dashboardimgid').style.display='block';
+}
+
+function isMobileScreen(){
+    return window.innerWidth<=768;
+}
